4b95a4eb86bebef067907914d19354ec98838460
GJW <320891162@qq.com>
2016-07-21 09:19:34 +0800

    修改宏

--- MVCProject/Main/Base/NavigationController.m
+++ MVCProject/Main/Base/NavigationController.m
@@ -9,18 +9,22 @@
 #import "NavigationController.h"
 
 @interface NavigationController ()
-
 @end
 
+#define kBarTintColor   [UIColor whiteColor]
+#define kTintColor      [UIColor blackColor]
+#define kTitleColor     [UIColor whiteColor]
+#define kItemTextColor  [UIColor blackColor]
+#define kItemTextFont   13
 @implementation NavigationController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    self.navigationBar.barTintColor = JWColor(19, 152, 244);
+    self.navigationBar.barTintColor = kBarTintColor;
     NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
-    textAttrs[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    textAttrs[NSForegroundColorAttributeName] = kTitleColor;
     self.navigationBar.titleTextAttributes = textAttrs;
-    self.navigationBar.tintColor = [UIColor blackColor];
+    self.navigationBar.tintColor = kTintColor;
     //将返回按钮的文字position设置不在屏幕上显示
     [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(NSIntegerMin, NSIntegerMin) forBarMetrics:UIBarMetricsDefault];
 }
@@ -31,15 +35,13 @@
     // 设置整个项目所有item的主题样式
     UIBarButtonItem *item = [UIBarButtonItem appearance];
     NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
-    textAttrs[NSForegroundColorAttributeName] = [UIColor colorWithRed:0.400 green:0.800 blue:1.000 alpha:1.000];
-    
-    textAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:13];
+    textAttrs[NSForegroundColorAttributeName] = kItemTextColor;
+    textAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:kItemTextFont];
     [item setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    
     // 设置不可用状态
     NSMutableDictionary *disableTextAttrs = [NSMutableDictionary dictionary];
-    disableTextAttrs[NSForegroundColorAttributeName] = [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:0.7];
-    disableTextAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:13];
+    disableTextAttrs[NSForegroundColorAttributeName] = kItemTextColor;
+    disableTextAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:kItemTextFont];
     [item setTitleTextAttributes:disableTextAttrs forState:UIControlStateDisabled];
 }
 /**
@@ -49,6 +51,7 @@
  */
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
     if (self.viewControllers.count > 0) {
+        //隐藏TabBarItem
         viewController.hidesBottomBarWhenPushed = YES;
     }
     [super pushViewController:viewController animated:animated];
--- MVCProject/Main/Base/TabBarController.m
+++ MVCProject/Main/Base/TabBarController.m
@@ -11,9 +11,9 @@
 #import "HomeVC.h"
 
 @interface TabBarController ()
-
 @end
-
+#define kSelectTextColor    [UIColor blackColor]
+#define kTextColor          [UIColor blackColor]
 @implementation TabBarController
 
 - (void)viewDidLoad {
@@ -27,7 +27,6 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
-
 /**
  *  添加一个子控制器
  *
@@ -37,21 +36,20 @@
  *  @param selectedImage 选中的图片
  */
 - (void)addChildVc:(UIViewController *)childVc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage{
-    // 设置子控制器的文字
-    childVc.title = title; // 同时设置tabbar和navigationBar的文字
-    // 设置子控制器的图片JWColor(36, 107, 246)
-    childVc.tabBarItem.image = [UIImage imageNamed:image];
+    // 设置子控制器的文字 - 同时设置tabbar和navigationBar的文字
+    childVc.title = title;
+    // 设置子控制器的图片
+    childVc.tabBarItem.image = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     // 设置文字的样式
-//    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
-//    textAttrs[NSForegroundColorAttributeName] = [UIColor blueColor];
-//    [childVc.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
+    textAttrs[NSForegroundColorAttributeName] = kTextColor;
+    [childVc.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
     //选中颜色
     NSMutableDictionary *selectTextAttrs = [NSMutableDictionary dictionary];
-    selectTextAttrs[NSForegroundColorAttributeName] = [UIColor blackColor];
+    selectTextAttrs[NSForegroundColorAttributeName] = kSelectTextColor;
     [childVc.tabBarItem setTitleTextAttributes:selectTextAttrs forState:UIControlStateSelected];
-    
-    //默认不显示文件 - 图片居中
+    //默认不显示标题 - 图片居中
     CGFloat offset = 5.0;
     childVc.tabBarItem.imageInsets = UIEdgeInsetsMake(offset, 0, -offset, 0);
     // 先给外面传进来的小控制器 包装 一个导航控制器
