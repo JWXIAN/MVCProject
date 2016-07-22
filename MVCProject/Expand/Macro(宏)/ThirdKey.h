86abeb1ecaacce314cff4dfe17ed3139e1f3ee4d
GJW <320891162@qq.com>
2016-07-22 11:12:31 +0800

    2016-07-22 11:12:08  添加常用分类

---
+++ MVCProject/Expand/Macro(宏)/ThirdKey.h
@@ -0,0 +1,16 @@
+//
+//  ThirdKey.h
+//  MVCProject
+//
+//  Created by GJW on 16/7/22.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#ifndef ThirdKey_h
+#define ThirdKey_h
+//微信
+#define kWeChatKey_ID @""
+//QQ
+#define kQQKey_ID @""
+
+#endif /* ThirdKey_h */
